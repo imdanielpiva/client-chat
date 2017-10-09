1c275ef6ea83e82beaba4df47abab1c491cb8442
daniel piva <imdanielpiva@gmail.com>
2017-10-09 15:52:05 -0300

    added working in progress server

---
+++ src/modules/chat/index.js
@@ -0,0 +1,38 @@
+const SseChannel = require('sse-channel');
+const express = require('express');
+const app = express();
+
+app.use(bodyParser.urlencoded({
+	extended: true
+}));
+app.use(bodyParser.json());
+
+const cnn = new SseChannel({
+	jsonEncode: true,
+	cors: {
+		origins: ['*']
+	}
+});
+
+let m = {
+	data: 'oi',
+	id: 0,
+	event: 'event',
+	retry: () => {
+		retry(10000000);
+	}
+}
+
+let id = 0;
+let clients = []
+
+app.get('/', (req, res) => {
+	cnn.addClient(req, res);
+	console.log('oi');
+	cnn.send(m);
+	console.log(req.query);
+});
+
+app.listen(3000, () => {
+	console.log('listening on localhost:3000')
+});
