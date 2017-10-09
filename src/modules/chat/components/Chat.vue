649e1713d16d21df0832a2d09838d2977ba9ca99
daniel piva <imdanielpiva@gmail.com>
2017-10-09 15:51:11 -0300

    changed component structure

---
+++ src/modules/chat/components/Chat.vue
@@ -0,0 +1,71 @@
+<template>
+  <div
+    class="q-message"
+    :class="{
+      'q-message-sent': sent,
+      'q-message-received': !sent
+    }"
+  >
+    <p v-if="label" class="q-message-label text-center" v-html="label"></p>
+    <div v-if="avatar" class="q-message-container row items-end no-wrap">
+      <slot name="avatar">
+        <img class="q-message-avatar" :src="avatar">
+      </slot>
+      <div class="column">
+        <div v-if="name" class="q-message-name" v-html="name"></div>
+        <div
+          v-for="(msg, index) in text"
+          :key="index"
+          class="q-message-text"
+          :class="messageClass"
+        >
+          <span class="q-message-text-content" :class="textClass">
+            <div v-html="msg"></div>
+            {{ index }}
+            <chat-stamp :options="{ stamp, sent, index }" />
+          </span>
+        </div>
+        <div v-if="!text || !text.length" class="q-message-text" :class="messageClass">
+          <span class="q-message-text-content" :class="textClass">
+            <slot></slot>
+            <chat-stamp :options="{ stamp, sent }" />
+          </span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ChatStamp from './ChatStamp';
+
+export default {
+  name: 'chat',
+  components: {
+    ChatStamp
+  },
+  props: {
+    sent: Boolean,
+    label: String,
+    bgColor: String,
+    textColor: String,
+    name: String,
+    avatar: String,
+    text: Array,
+    stamp: String,
+    spinner: Object
+  },
+  computed: {
+    textClass () {
+      if (this.textColor) {
+        return `text-${this.textColor}`
+      }
+    },
+    messageClass () {
+      if (this.bgColor) {
+        return `text-${this.bgColor}`
+      }
+    }
+  }
+}
+</script>
