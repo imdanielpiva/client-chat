641eabae4fd791497aff55843a0aacebcd84199e
daniel piva <imdanielpiva@gmail.com>
2017-10-09 15:50:28 -0300

    initial commit

---
+++ src/modules/chat/components/ChatSpinner.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="q-message-text" :class="messageClass">
+    <span class="q-message-text-content" :class="textClass">          
+      <q-spinner-dots size="2em"/>
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chat-spinner',
+  props: {
+    textClass: Object,
+    textColor: String,
+    bgColor: String
+  },
+  computed: {
+    textClass () {
+      if (this.textColor) {
+        return `text-${this.textColor}`
+      }
+    },
+    messageClass () {
+      if (this.bgColor) {
+        return `text-${this.bgColor}`
+      }
+    }
+  }
+}
+</script>
---
+++ src/modules/chat/components/ChatStamp.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- <div class="q-message-stamp"></div> -->
+    <div
+      style="width:100%;"
+      class="q-message-stamp animate-scale"
+    >
+      {{ options.stamp }}
+      <i
+        aria-hidden="false"
+        class="q-icon material-icons text-white"
+      >
+      done_all
+      </i>
+    </div>
+    {{ options }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'chatStamp',
+  props: ['options'],
+  methods: {
+    wasItSent() {
+
+    },
+    wasItSeen() {
+
+    },
+    wasItDelivered() {
+      
+    }
+  }
+}
+</script>
+
+<style scoped>
+.stamp {
+  margin-left: 4px;
+  font-size: 1.2em;
+}
+</style>
