d82db503c932e73bffe875bad76ef714503eb453
daniel piva <imdanielpiva@gmail.com>
2017-10-11 15:51:02 -0300

    fixed message id and date properties

--- src/modules/chat/options/methods.js
+++ src/modules/chat/options/methods.js
@@ -46,6 +46,7 @@ export const methods = {
     this.$store.commit('chat/NOTIFY_CLIENT_IS_TYPING');
   },
   submit() {
+    this.id = this.generateUId();
     if (this.$q.platform.is.mobile) {
       this.$scrollTo('#message', 600, {
         container: '#chat',
@@ -57,30 +58,31 @@ export const methods = {
     
     if (this.name && this.email && this.phone && this.message.replace(/\n/gi, '')) {
       this.$store.commit('chat/PUSH_ONLY_MESSAGE', {
-        id: this.generateUId(),
+        id: this.id,
         name: 'You',
         text: [this.message],
         avatar: '../../statics/me.png',
         side: true,
         info: {
           type: 1,
-          stamp: new Date().getTime(),
+          stamp: this.getHour,
           sent: false,
           seen: false,
           delivered: false
         }
       });
-      this.$store.commit('chat/IS_SUPPORT_TYPING');    
+      this.$store.commit('chat/QUEUE_UNREAD_MESSAGES', this.messages);
+      this.$store.commit('chat/IS_SUPPORT_TYPING');
       setTimeout(() => {
         this.$store.commit('chat/PUSH_ONLY_MESSAGE',{
-          id: this.generateUId(),
+          id: this.id + 'oi',
           name: 'SatTrack',
           text: ['Hey, we\'re gonna e-mail you or send a message to you soon, then stick around!'],
           avatar: '../../statics/me.png',
           side: false,
           info: {
             type: 0,
-            stamp: new Date().getTime(),
+            stamp: this.getHour,
             sent: false,
             seen: false,
             delivered: false
@@ -100,7 +102,7 @@ export const methods = {
         cancelable: false
       });
     }
-
+    // this.id = undefined;
     this.$refs.message.focus(); 
   },
   openPopupStatus() {
@@ -135,6 +137,13 @@ export const methods = {
     if (this.name && this.email && this.$q.platform.is.mobile) {
       return this.isInputHided = false;
     }
+  },
+  fixZero(time) {
+    if (time < 10) {
+      time = "0" + time;
+    }
+    
+    return time;
   },
   ...mapActions({ 
     send: 'PUSH_ONLY_MESSAGE'
