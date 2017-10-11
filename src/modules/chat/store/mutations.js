668b14d92656f4ec02f8b6b21bdac9340b404716
daniel piva <imdanielpiva@gmail.com>
2017-10-11 13:42:46 -0300

    added validation for message object before pushing it and added queue to unread messages

--- src/modules/chat/store/mutations.js
+++ src/modules/chat/store/mutations.js
@@ -1,7 +1,35 @@
 import * as TYPES from './mutation-types';
 import { state } from './state';
 
+function isMessageValid (message) {
+  if (!message) return false;
+  if (typeof message === 'object') {
+    if (message.id === undefined || typeof message.id === 'undefined' || message.id === undefined) return false;
+    if (message.text === undefined || message.text === '' || message.text === ' ') return false;
+    if (Array.isArray(message.text) === false || message.text.length === 0) return false;
+    if (message.name === '' || message.name === undefined) return false;
+    if (message.info.type === undefined || typeof message.info.type !== 'number') return false;
+
+    return true;
+  }
+
+  return false;
+}
+
+function isMessageAlreadyQueued() {
+  let arr = [...arguments]
+    
+  return arr.shift().filter( y => arr.every(
+    x => x.some( j => j.id !== y.id
+  )));
+}
+
 export default {
+  [TYPES.QUEUE_UNREAD_MESSAGES](state, messages){
+     messages.forEach(function(message) {
+       if (message.info.sent === false) state.toBeRead.push(message);
+    }, this);
+  },
   [TYPES.PUSH_WELCOME_MESSAGE](state, welcomeMessage) {
     if (state.welcomeMessage === false) {
       state.welcomeMessage = true;
@@ -11,9 +39,10 @@ export default {
     }
   },
 	[TYPES.PUSH_ONLY_MESSAGE](state, message) {
-    if (message.name !== '' && Array.isArray(message.text) === true) {
+    if (isMessageValid(message)) {
       state.messages.push(message);
-    }    
+      state.message = '';
+    }
 	},
 	[TYPES.PUSH_CUSTOMER_INFO](state, data) {
 		state.customer.name = data.name;
